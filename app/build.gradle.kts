2d1e54f316d0cae41befc665eb5b2707cf76864b
hzrhscn <hazarhascan@gmail.com>
2024-01-06 15:43:29 +0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+}
+
+android {
+    namespace = "com.hzrhscn.myapplication"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.hzrhscn.myapplication"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures{
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.vision.common)
+    implementation(libs.play.services.mlkit.text.recognition.common)
+    implementation(libs.play.services.mlkit.text.recognition)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    val fragment_version = "1.6.1"
+
+    // Kotlin
+    implementation("androidx.fragment:fragment-ktx:$fragment_version")
+}
---
+++ app/src/main/java/com/hzrhscn/myapplication/ResultActivity.kt
@@ -0,0 +1,49 @@
+package com.hzrhscn.myapplication
+
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
+import android.os.Build.VERSION
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import com.hzrhscn.myapplication.databinding.ActivityResultBinding
+
+class ResultActivity : AppCompatActivity() {
+
+    private  lateinit var  binding: ActivityResultBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val (bitmap, uri) = getImageFromIntent()
+        setImage(uri, bitmap)
+
+        binding.buttonBack.setOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    private fun setImage(uri: String?, bitmap: Bitmap?) {
+        binding.apply {
+            imageView.setImageDrawable(null)
+            if (uri != null) {
+                imageView.setImageURI(Uri.parse(uri))
+            } else {
+                imageView.setImageBitmap(bitmap)
+            }
+        }
+    }
+
+    private fun getImageFromIntent(): Pair<Bitmap?, String?> {
+        val intent = getIntent()
+        val bitmap = if (VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) intent.getParcelableExtra("bitmapimage", Bitmap::class.java)
+        else intent.getParcelableExtra("bitmapimage")
+
+        val uri = intent.getStringExtra("uriimage")
+        return Pair(bitmap, uri)
+    }
+
+}
